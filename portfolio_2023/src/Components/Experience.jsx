a95271591e296aded38a49c24d6fb978778ee587
Steven DeLitta <sdelitta@gmail.com>
2023-08-07 10:19:08 -0400

    Added content to Projects and Experience sections

--- portfolio_2023/src/Components/Experience.jsx
+++ portfolio_2023/src/Components/Experience.jsx
@@ -7,19 +7,20 @@ function Experience() {
       <h2>Work Experience</h2>
       <div className="row">
         <div className="col-lg-6">
-          <h4>Job Title 1</h4>
-          <h5>Company Name 1</h5>
-          <h6>Start Date - End Date</h6>
-          <p>A brief description of your role and responsibilities...</p>
+          <h4>Web Developer</h4>
+          <h5>Self Learning</h5>
+          <h6>2021 - Present</h6>
+          <p>Over the past two years, I've been focused on learning web development, starting from the basics of Javascript to creating full-stack web applications. I've completed the General Assembly Software Engineering bootcamp, and have built several projects using JavaScript, React, Bootstrap, and more while utilizing the latest SEO strategies and Ai technologies.</p>
         </div>
         <div className="col-lg-6">
-          <h4>Job Title 2</h4>
-          <h5>Company Name 2</h5>
-          <h6>Start Date - End Date</h6>
-          <p>A brief description of your role and responsibilities...</p>
+          <h4>Non-Tech Work Experience</h4>
+          <h5>Select Services</h5>
+          <h6>2022 - Present</h6>
+          <p>As a Direct Service Provider for individuals who suffer with epilepsy, I provide guidance and aid to those who are having difficulty adapting to life outside their home. In this role, communication is critical. I've found progress cannot be made until trust and a bond is made through attentive communication and understanding.</p>
+
+          <p>The role of a DSP also requires the ability to assess and adapt to new goals and challenges on a daily basis. For instance, the individual may be resistant to the scheduled plan for the day and they may have trouble communicating their apprehension. Recognizing subtle cues, responding kindly and offering solutions in the moment is necessary to maintain their trust.</p>
         </div>
       </div>
-      {/* Add more rows here for more jobs */}
     </div>
   );
 }
--- portfolio_2023/src/Components/Projects.jsx
+++ portfolio_2023/src/Components/Projects.jsx
@@ -17,43 +17,45 @@ function Projects() {
         <Carousel.Item>
           <img className="d-block w-100" src="/Assets/Gothic.png" alt="Gothic Getaways" />
           <Carousel.Caption>
-            <h3><a href="http://www.GothicGetaways.com" target="_blank" rel="noopener noreferrer">Gothic Getaways</a></h3>
-            <div>
-              <p>Gothic Getaways, launched in June 2023, is a testament to the rapid strides I've made in web development and digital marketing. This intriguing blog site delves into the fascinating niche of gothic destinations and attractions, offering readers unique insights and engaging content. What sets Gothic Getaways apart is its foundation in solid SEO practices, drawing upon my extensive experience with tools like Google Analytics and Google Console to drive site traffic.</p>
-            </div>
-            <Button variant="primary" onClick={() => handleShow('GothicGetaways')}>
+          <div className="project_info">
+              <h3><a href="http://www.GothicGetaways.com" target="_blank" rel="noopener noreferrer">Gothic Getaways</a></h3>
+              <p>Gothic Getaways, launched in June 2023, is a testament to the rapid strides I've made in web development and digital marketing. The blog site features beautiful images I generated using Midjourney and finely tuned content curated with the help of Ai technology.</p>
+              <Button variant="primary" onClick={() => handleShow('PetCarePurrsuit')}>
               Read More
             </Button>
+            </div>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img className="d-block w-100" src="/Assets/PetCare.png" alt="Pet Care Purrsuit" />
           <Carousel.Caption>
-            <h3><a href="http://www.PetCarePurrsuit.com" target="_blank" rel="noopener noreferrer">Pet Care Purrsuit</a></h3>
-            <div>
-              <p>Pet Care Purrsuit is a passion project launched in January 2023 that marries my love for pets with my skills in web development and digital marketing. The site, built using JavaScript and React, serves as a comprehensive resource for pet owners, offering a range of articles focused on pet care and product recommendations.</p>
-            </div>
+          <div className="project_info">
+            <h3><a href="http://www.PetCarePurrsuit.com" target="_blank" rel="noopener noreferrer">Pet Care Purrsuit</a></h3>          
+            <p>Pet Care Purrsuit is a passion project launched in January 2023 that marries my love for pets with my skills in web development and digital marketing. The site, built using JavaScript and React, serves as a comprehensive resource for pet owners, offering a range of articles focused on pet care and product recommendations.</p>
             <Button variant="primary" onClick={() => handleShow('PetCarePurrsuit')}>
               Read More
             </Button>
+          </div>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img className="d-block w-100" src="/Assets/Boo.png" alt="Boo" />
           <Carousel.Caption>
-            <h3><a href="https://boocursor.netlify.app/" target="_blank" rel="noopener noreferrer">Super Mario Animated Cursor</a></h3>
-            <p>The adorable spectre from the Super Mario franchise, Boo, is known for giving chase when our heroes back is turned, but then cowardly freezes in its tracks when Mario confronts it. This "mousemove" animation is meant to mimic those behaviors as well as change expressions depending on when the user moves their cursor.</p>
+            <div className="project_info">
+              <h3><a href="https://boo-cursor.web.app/" target="_blank" rel="noopener noreferrer">Super Mario Animated Cursor</a></h3>
+              <p>The adorable spectre from the Super Mario franchise, Boo, is known for giving chase when our heroes back is turned, but then cowardly freezes in its tracks when Mario confronts it. This "mousemove" animation is meant to mimic those behaviors as well as change expressions depending on when the user moves their cursor.</p>
+            </div>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
-      <Modal show={showModal !== null} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title></Modal.Title>
+      <Modal className="modal" show={showModal !== null} onHide={handleClose}>
+        <Modal.Header className="modal_header" closeButton>
+          <Modal.Title className="modal_title"></Modal.Title>
         </Modal.Header>
-        <Modal.Body>
+        <Modal.Body className="modal_body">
           {showModal === 'GothicGetaways' && (
             <div id="modal-background">
-            <p>Gothic Getaways, launched in June 2023, is a testament to the rapid strides I've made in web development and digital marketing. This intriguing blog site delves into the fascinating niche of gothic destinations and attractions, offering readers unique insights and engaging content. What sets Gothic Getaways apart is its foundation in solid SEO practices, drawing upon my extensive experience with tools like Google Analytics and Google Console to drive site traffic.</p>
+            <p>Gothic Getaways, launched in June 2023, is a testament to the rapid strides I've made in web development and digital marketing. The blog site features beautiful images I generated using Midjourney and finely tuned content curated with the help of Ai technology. The articles delve into the fascinating niche of gothic destinations and attractions, offering readers unique insights and engaging content. What sets Gothic Getaways apart is its foundation in solid SEO practices, drawing upon my extensive experience with tools like Google Analytics and Google Console to drive site traffic.</p>
 
             <p>In the creation of Gothic Getaways, I was able to significantly reduce the development time compared to my previous project, Pet Care Purrsuit. This accomplishment speaks to my growing proficiency in building efficient, high-performance websites. Like Pet Care Purrsuit, Gothic Getaways also utilizes affiliate marketing and innovative AI image generation technology, Midjourney, to enhance user engagement.</p>
 
@@ -75,8 +77,8 @@ function Projects() {
           )}
           {/* Add more lines as needed for each project */}
         </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
+        <Modal.Footer className="modal_footer">
+          <Button className="modal_button" variant="secondary" onClick={handleClose}>
             Close
           </Button>
         </Modal.Footer>
